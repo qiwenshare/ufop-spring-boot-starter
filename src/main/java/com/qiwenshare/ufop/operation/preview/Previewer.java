eb23b80dffd6e40263fd16a96216b2feb05ed272
MAC <1162714483@qq.com>
2023-08-31 19:43:04 +0800

    1.2.9

--- src/main/java/com/qiwenshare/ufop/operation/preview/Previewer.java
+++ src/main/java/com/qiwenshare/ufop/operation/preview/Previewer.java
@@ -1,6 +1,5 @@
 package com.qiwenshare.ufop.operation.preview;
 
-import cn.hutool.http.HttpUtil;
 import com.qiwenshare.common.operation.ImageOperation;
 import com.qiwenshare.ufop.domain.ThumbImage;
 import com.qiwenshare.ufop.exception.operation.PreviewException;
@@ -14,9 +13,6 @@ import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 @Slf4j
 @Data
@@ -28,61 +24,6 @@ public abstract class Previewer {
 
     public void imageThumbnailPreview(HttpServletResponse httpServletResponse, PreviewFile previewFile) {
         String fileUrl = previewFile.getFileUrl();
-
-        if(fileUrl.startsWith("http://") || fileUrl.startsWith("https://")) {
-            String[] arr = fileUrl.replace("http://", "").replace("https://", "").split("/");
-            String name = arr[0];
-            String icoUrl = findIco(fileUrl);
-
-            File cacheFile = UFOPUtils.getCacheFile(UFOPUtils.getUploadFileUrl(name, "ico"));
-            if (cacheFile.exists()) {
-                FileInputStream fis = null;
-                OutputStream outputStream = null;
-                try {
-                    fis = new FileInputStream(cacheFile);
-                    outputStream = httpServletResponse.getOutputStream();
-                    IOUtils.copy(fis, outputStream);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    IOUtils.closeQuietly(fis);
-                    IOUtils.closeQuietly(outputStream);
-                }
-            } else {
-                URL url = null;
-                InputStream in = null;
-                OutputStream outputStream = null;
-                try {
-                    url = new URL(icoUrl);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    InputStream inputstream = connection.getInputStream();
-                    try {
-                        outputStream = httpServletResponse.getOutputStream();
-                        in = ImageOperation.thumbnailsImageForScale(inputstream, cacheFile, 50);
-                        IOUtils.copy(in, outputStream);
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        IOUtils.closeQuietly(in);
-                        IOUtils.closeQuietly(inputstream);
-                        IOUtils.closeQuietly(outputStream);
-                        if (previewFile.getOssClient() != null) {
-                            previewFile.getOssClient().shutdown();
-                        }
-                    }
-                } catch (MalformedURLException e) {
-
-                    log.error("MalformedURLException, url is {}", icoUrl );
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    log.error("IOException, url is {}", icoUrl );
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return ;
-        }
 
 
         boolean isVideo = UFOPUtils.isVideoFile(FilenameUtils.getExtension(fileUrl));
@@ -139,7 +80,12 @@ public abstract class Previewer {
         }
     }
 
+    @Deprecated
     public void imageOriginalPreview(HttpServletResponse httpServletResponse, PreviewFile previewFile) {
+        preview(httpServletResponse, previewFile);
+    }
+
+    public void preview(HttpServletResponse httpServletResponse, PreviewFile previewFile) {
 
         InputStream inputStream = null;
 
@@ -163,22 +109,53 @@ public abstract class Previewer {
         }
     }
 
-    private static String findIco(String navUrl) {
-        String body = HttpUtil.createGet(navUrl).execute().toString();
-        String str = body.split("favicon\\d{0,3}.ico")[0];
-        int http = str.indexOf("https://",str.length()-100);
-        if(http==-1){
-            http = str.indexOf("http://",str.length()-100);
-        }
-        if(http==-1){
-            //说明没有指定 走拼接逻辑
-            int i = navUrl.indexOf("/",8);//获取网址 拼接 favicon.ico
-            if(i>0){
-                navUrl = navUrl.substring(0, i);
-            }
-        }else {
-            navUrl = str.substring(http);
-        }
-        return navUrl+"/favicon.ico";
-    }
+//    public void videoPreview(HttpServletResponse httpServletResponse, PreviewFile previewFile) {
+//        String fileUrl = previewFile.getFileUrl();
+//
+//
+//        String thumbnailImgUrl = fileUrl.replace("." + FilenameUtils.getExtension(fileUrl), ".mp4");
+//
+//        InputStream inputStream = null;
+//
+//        OutputStream outputStream = null;
+//
+//        File cacheFile = UFOPUtils.getCacheFile(thumbnailImgUrl);
+//        try {
+//            if (!cacheFile.exists()) {
+//
+//                inputStream = getInputStream(previewFile);
+//                FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(inputStream);
+//
+//                Frame captured_frame = null;
+//                FFmpegFrameRecorder recorder = null;
+//
+//
+//                frameGrabber.start();
+//                recorder = new FFmpegFrameRecorder(cacheFile, frameGrabber.getImageWidth(), frameGrabber.getImageHeight(), frameGrabber.getAudioChannels());
+//                recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264); //avcodec.AV_CODEC_ID_H264  //AV_CODEC_ID_MPEG4
+//                recorder.setFormat("mp4");
+//                recorder.setFrameRate(frameGrabber.getFrameRate());
+//                //recorder.setSampleFormat(frameGrabber.getSampleFormat()); //
+//                recorder.setSampleRate(frameGrabber.getSampleRate());
+//
+//                recorder.setAudioChannels(frameGrabber.getAudioChannels());
+//                recorder.setFrameRate(frameGrabber.getFrameRate());
+//                recorder.start();
+//                while ((captured_frame = frameGrabber.grabFrame()) != null) {
+//                    try {
+//                        recorder.setTimestamp(frameGrabber.getTimestamp());
+//                        recorder.record(captured_frame);
+//                    } catch (Exception e) {
+//                    }
+//                }
+//                recorder.stop();
+//                recorder.release();
+//                frameGrabber.stop();
+//
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
 }
